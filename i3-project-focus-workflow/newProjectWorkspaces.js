f2c5796a1db95f13a7d34fa4716b271cc08d0b3f
sainathadapa <adapasainath@gmail.com>
2015-02-19 23:02:23 +0530

    bug to be fixed in newProjectWorkspace but atleast found out a way to run it using a key binding

--- i3-project-focus-workflow/newProjectWorkspaces.js
+++ i3-project-focus-workflow/newProjectWorkspaces.js
@@ -3,38 +3,41 @@
 var exec = require('child_process').exec,
 _ = require('lodash');
 
-var projectName = process.argv[2];
+var childProc = exec("echo 'assets'|dmenu", function(error1, stdout1, stderr1) {
+	var projectName = stdout1;
 
-exec('i3-msg -t get_workspaces', function (error, stdout, stderr) {
-	var wkList = JSON.parse(stdout);
+	exec('i3-msg -t get_workspaces', function (error, stdout, stderr) {
+		var wkList = JSON.parse(stdout);
 
-	var allOutputs = _.uniq(_.pluck(wkList, 'output'));
+		var allOutputs = _.uniq(_.pluck(wkList, 'output'));
 
-	var maxWk = _.max(_.pluck(wkList,'num'));
+		var maxWk = _.max(_.pluck(wkList,'num'));
 
-	var commandToRun = '';
+		var commandToRun = '';
 
-	var wkPrefix = 'proj' + projectName.toUpperCase();
+		var wkPrefix = 'proj' + projectName.toUpperCase();
 
-	for (var i = 1; i <= allOutputs.length; i++) {
-		var oneWKonCurrentOutput = _.filter(wkList, function(x) {
-			return(x.output == allOutputs[i - 1]);
-		});
+		for (var i = 1; i <= allOutputs.length; i++) {
+			var oneWKonCurrentOutput = _.filter(wkList, function(x) {
+				return(x.output == allOutputs[i - 1]);
+			});
 
-		if(oneWKonCurrentOutput[0].focused===false) {
-			oneWKonCurrentOutput = oneWKonCurrentOutput[0].name;
-			commandToRun = commandToRun + 'workspace ' + oneWKonCurrentOutput + '; ';
+			if(oneWKonCurrentOutput[0].focused===false) {
+				oneWKonCurrentOutput = oneWKonCurrentOutput[0].name;
+				commandToRun = commandToRun + 'workspace ' + oneWKonCurrentOutput + '; ';
+			}
+
+			var currentWKName = (maxWk + i) + ':' + wkPrefix + '-' + i;
+
+			commandToRun = commandToRun + 'workspace ' + currentWKName + '; ';
+			commandToRun = commandToRun + 'move workspace ' + currentWKName + ' to output ' + allOutputs[i - 1] + '; ';
 		}
 
-		var currentWKName = (maxWk + i) + ':' + wkPrefix + '-' + i;
+		commandToRun = 'i3-msg "' + commandToRun + '"';
 
-		commandToRun = commandToRun + 'workspace ' + currentWKName + '; ';
-		commandToRun = commandToRun + 'move workspace ' + currentWKName + ' to output ' + allOutputs[i - 1] + '; ';
-	}
+		console.log(commandToRun);
 
-	commandToRun = 'i3-msg "' + commandToRun + '"';
-
-	console.log(commandToRun);
-
-	exec(commandToRun);
+		exec(commandToRun);
+	});
 });
+
