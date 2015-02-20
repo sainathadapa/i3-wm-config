cff2fa738334613ff80c021ca2dd40881b941823
Sainath Adapa <sainathadapa@users.noreply.github.com>
2015-02-20 09:02:25 +0530

    using zenity instead of dmenu to take input

--- i3-project-focus-workflow/newProjectWorkspaces.js
+++ i3-project-focus-workflow/newProjectWorkspaces.js
@@ -3,8 +3,14 @@
 var exec = require('child_process').exec,
 _ = require('lodash');
 
-var childProc = exec("echo 'assets'|dmenu", function(error1, stdout1, stderr1) {
+var childProc = exec("zenity --entry --title=I3 --text='Start a new project with the name:'", function(error1, stdout1, stderr1) {
 	var projectName = stdout1;
+	
+	projectName = projectName.replace(/(\r\n|\n|\r)/gm,"");
+
+	if(projectName === undefined || projectName.length == 0) {
+		return undefined;
+	}
 
 	exec('i3-msg -t get_workspaces', function (error, stdout, stderr) {
 		var wkList = JSON.parse(stdout);
@@ -17,12 +23,13 @@ var childProc = exec("echo 'assets'|dmenu", function(error1, stdout1, stderr1) {
 
 		var wkPrefix = 'proj' + projectName.toUpperCase();
 
+
 		for (var i = 1; i <= allOutputs.length; i++) {
 			var oneWKonCurrentOutput = _.filter(wkList, function(x) {
 				return(x.output == allOutputs[i - 1]);
 			});
 
-			if(oneWKonCurrentOutput[0].focused===false) {
+			if (oneWKonCurrentOutput[0].focused === false) {
 				oneWKonCurrentOutput = oneWKonCurrentOutput[0].name;
 				commandToRun = commandToRun + 'workspace ' + oneWKonCurrentOutput + '; ';
 			}
@@ -35,9 +42,9 @@ var childProc = exec("echo 'assets'|dmenu", function(error1, stdout1, stderr1) {
 
 		commandToRun = 'i3-msg "' + commandToRun + '"';
 
-		console.log(commandToRun);
+		// exec('zenity --notification --text=' + JSON.stringify(commandToRun));
+		// console.log(commandToRun);
 
 		exec(commandToRun);
 	});
 });
-
