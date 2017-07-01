84da2d67d45a37fa436355fb4b2704839ff4bdc6
Sainath Adapa <sainathadapa@users.noreply.github.com>
2017-07-01 23:46:42 +0530

    removed scripts which are no longer being used

--- newwk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WKNAME=$1
-echo workspace name given is $WKNAME
-if i3-msg -t get_workspaces | jq ".[] | .name" | grep -q -w $WKNAME; then
-  i3-msg "workspace $WKNAME"
-else
-  i3-msg "workspace $WKNAME; append_layout ~/.i3/empty_workspace.json"
-fi
