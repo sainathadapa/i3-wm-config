c6093cb314a415ae6e8d3c4736e9f905a3ad5461
Sainath Adapa <adapasainath@gmail.com>
2018-03-06 10:52:48 +0100

    misc changes

--- volume_increase.sh
+++ volume_increase.sh
@@ -5,4 +5,4 @@ sink=`pactl list short sinks | grep RUNNING | cut -f1`
 SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
 NOW=$( pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
 
-notify-send "Volume $NOW" -t 500
+notify-send "Volume+ $NOW" -t 500
