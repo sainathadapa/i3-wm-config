16943c7049d3082c4467d93d9f5d22846d04e44e
Sainath Adapa <adapasainath@gmail.com>
2018-07-12 19:50:53 +0530

    brightness script change

--- brightness_decrease.sh
+++ brightness_decrease.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-light -U 5
 sink=`light -G` 
+sink=${sink%.*}
+if [ $sink -gt 10 ]
+then
+  light -U 5
+else
+  light -U 1
+fi
+sink=`light -G` 
+sink=${sink%.*}
 notify-send $sink -t 500
 
--- brightness_increase.sh
+++ brightness_increase.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-light -A 5
-sink=`light -G`
+sink=`light -G` 
+sink=${sink%.*}
+if [ $sink -gt 10 ]
+then
+  light -A 5
+else
+  light -A 1
+fi
+sink=`light -G` 
+sink=${sink%.*}
 notify-send $sink -t 500
+
 
