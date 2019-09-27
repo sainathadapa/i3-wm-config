a5c1d77b0b0a3c5529a3d62c3ec0b6c7238549f5
Sainath Adapa <sainathadapa@users.noreply.github.com>
2019-09-27 19:21:38 +0200

    update

---
+++ utils/map_touchscreen_to_laptop_screen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xinput --map-to-output 'Wacom HID 5168 Pen stylus' eDP1
+xinput --map-to-output 'Wacom HID 5168 Finger touch' eDP1
+xinput --map-to-output 'Wacom HID 5168 Pen eraser' eDP1
+
